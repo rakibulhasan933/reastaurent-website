c65c45f32b2ed9b072c7796721e446ca2729b669
RAKIBUL <rakibulmd933@gmail.com>
2022-12-02 20:35:14 +0600

    [ADD] navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Navbar = () => {
 	return (
-		<div className="bg-orange-500 navbar">
+		<div className="bg-orange-400 navbar">
 			<div className="navbar-start">
 				<div className="dropdown">
 					<label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -13,14 +13,14 @@ const Navbar = () => {
 						<li><a>Home</a></li>
 						<li><a>Products</a></li>
 						<li><a>Menu</a></li>
-						<li className=' bg-base-100'><a><Image src='/img/logo.png' alt='logo' width='50' height='50' /> </a></li>
+						<li className='bg-orange-400 rounded'><a><Image src='/img/logo.png' alt='logo' width='50' height='50' /> </a></li>
 						<li><a>Events</a></li>
 						<li><a>Blogs</a></li>
 						<li><a>Contacts</a></li>
 					</ul>
 				</div>
 				<div className="flex content-center m-2">
-					<div>
+					<div className='p-2 border rounded-full bg-slate-100'>
 						<Image src="/img/telephone.png" alt='logo' width="32" height="32" />
 					</div>
 					<div className='flex-col px-2'>
@@ -34,14 +34,14 @@ const Navbar = () => {
 					<li><a>Home</a></li>
 					<li><a>Products</a></li>
 					<li><a>Menu</a></li>
-					<li><a><Image src='/img/logo.png' alt='logo' width='60' height='60' /> </a></li>
+					<li><a><Image src='/img/logo.png' alt='logo' width='100' height='100' /> </a></li>
 					<li><a>Events</a></li>
 					<li><a>Blogs</a></li>
 					<li><a>Contacts</a></li>
 				</ul>
 			</div>
-			<div className="navbar-end">
-				<a className="btn"><Image src='/img/cart.png' alt='cart' width="40" height="40" /> </a>
+			<div className="mx-3 navbar-end">
+				<a><Image src='/img/cart.png' alt='cart' width="40" height="40" /> </a>
 			</div>
 		</div>
 	);
